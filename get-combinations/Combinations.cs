cfbd9845ff2bcdb73bd0b059cb5b8356454e972e
André Rodrigues <biogenethik@hotmail.com>
2025-03-27 20:29:45 +0000

    upload solution

---
+++ get-combinations/Combinations.cs
@@ -0,0 +1,36 @@
+﻿internal static class Combinations
+{
+    internal static IEnumerable<IEnumerable<object>> GetAllCombinations(this IEnumerable<object> elements)
+    {
+        var response = new List<IEnumerable<object>>();
+
+        for (int combinationSize = 1; combinationSize <= elements.Count(); combinationSize++)
+        {
+            var combinationAux = new List<object>();
+
+            GetAllCombinations(elements, combinationSize, combinationAux, 0, response);
+        }
+
+        return response;
+    }
+
+    internal static void GetAllCombinations(IEnumerable<object> elements,
+                                           int combinationSize,
+                                           List<object> combinationAux,
+                                           int index,
+                                           List<IEnumerable<object>> combinations)
+    {
+        if (combinationAux.Count == combinationSize)
+        {
+            combinations.Add(combinationAux);
+            return;
+        }
+
+        for (int i = index; i < elements.Count(); i++)
+        {
+            combinationAux.Add(elements.ElementAt(i));
+            GetAllCombinations(elements, combinationSize, combinationAux, i + 1, combinations);
+            combinationAux.RemoveAt(combinationAux.Count - 1);
+        }
+    }
+}
---
+++ get-combinations/Program.cs
@@ -0,0 +1,7 @@
+﻿List<object> elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+DateTime start = DateTime.Now;
+IEnumerable<IEnumerable<object>> combinations = elements.GetAllCombinations();
+DateTime end = DateTime.Now;
+
+Console.WriteLine($"[{combinations.Count()}] combinations found on [{(end - start).TotalMilliseconds}] milliseconds!");
